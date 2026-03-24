8ea8719ebbd755f1f97576422d115285ec79e9ec
HamaguchiRyota <52881627+Hamaryo226@users.noreply.github.com>
2026-03-24 19:01:29 +0900

    OGPの対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  // TODO: デプロイ先のURLに変更してください
+  site: 'https://hamaryo.dev',
+
   vite: {
     css: {
       devSourcemap: true,
